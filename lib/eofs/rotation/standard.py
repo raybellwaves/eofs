776bebe9d6558e4257ab5243fa602ee14e0ff225
raybellwaves <rjbell1987@gmail.com>
2018-04-15 15:48:20 -0400

    add Barnston and Livezey paper

--- lib/eofs/rotation/standard.py
+++ lib/eofs/rotation/standard.py
@@ -1,6 +1,7 @@
 # Notes:
 # VARIMAX rotation algorithm:
-# Richman, M. B., 1986: Rotation of principal components. J. Climatol, 6, 293–335
+# Richman, M. B., 1986: Rotation of principal components. J. Climatol, 6, 293–335. https://rmets.onlinelibrary.wiley.com/doi/pdf/10.1002/joc.3370060305 (need to log in. TODO: Find pdf elsewhere)
+# Barnston and Livezey, 1987: Classification, Seasonality and Persistence of Low-Frequency Atmospheric Circulation Patterns. MWR, 115, 1093-1125. https://journals.ametsoc.org/doi/pdf/10.1175/1520-0493%281987%29115%3C1083%3ACSAPOL%3E2.0.CO%3B2
 
 # Methods:
 # https://stackoverflow.com/questions/17628589/perform-varimax-rotation-in-python-using-numpy
