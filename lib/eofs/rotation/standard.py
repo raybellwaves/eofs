b825f4adec2869560f08aceb464aa4c20d5eae02
raybellwaves <rjbell1987@gmail.com>
2018-04-15 15:34:30 -0400

    add computing methods

--- lib/eofs/rotation/standard.py
+++ lib/eofs/rotation/standard.py
@@ -1,3 +1,9 @@
 # Notes:
 # VARIMAX rotation algorithm:
 # Richman, M. B., 1986: Rotation of principal components. J. Climatol, 6, 293–335
+
+# Methods:
+# https://stackoverflow.com/questions/17628589/perform-varimax-rotation-in-python-using-numpy
+# https://en.wikipedia.org/wiki/Talk:Varimax_rotation
+# https://github.com/bmcmenamin/fa_kit/blob/master/fa_kit/rotation.py
+# https://github.com/rossfadely/consomme/blob/master/consomme/rotate_factor.py
